1a1cbd3987270e5819c8d184971a6b3811825c07
Timo Suomela <timo.suomela@reaktor.com>
2024-02-23 16:57:24 +0200

    Extract green build tagging to script

---
+++ scripts/tag-green-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+readonly repo="$(cd "$(dirname "$0")/.." && pwd)"
+source "${repo}/scripts/lib/common-functions.sh"
+
+function main {
+  local -r env=$(parse_env_from_script_name)
+  git tag "green-${env}"
+  git tag "green-${env}-${date +%s}"
+  git push --tags --force origin $(git branch --show-current)
+}
+
+main "$@"
