36b11117839f6b577ccc402fcba7a8c6112d02f9
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-11-29 12:45:30 +0200

    OPHYK-380 Schedule Trivy run regularly on CodeBuild

---
+++ run-trivy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+readonly repo="$(cd "$(dirname "$0")" && pwd)"
+source "${repo}/scripts/lib/common-functions.sh"
+readonly node_version=$(cat "$repo/.nvmrc")
+
+function main {
+  cd "$repo"
+  npm_ci_if_package_lock_has_changed
+  cd "$repo/docs"
+  require_docker
+  init_nodejs
+  npx ts-node holes.ts
+}
+
+main "$@"
