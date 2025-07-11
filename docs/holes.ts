0729b2cb2a786bd8f6c6d0e169e4d8054eb15da8
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-07-11 10:46:31 +0300

    Just load the script after content

--- docs/holes.ts
+++ docs/holes.ts
@@ -294,18 +294,6 @@ async function generateReportPage(title: string, viewName: string, repositories:
           .low { background-color: cyan; color: black; }
           a { color: white; }
       </style>
-      <script type="text/javascript" defer>
-          const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-          const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
-              v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-              )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-          document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-              const table = th.closest('table');
-              Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-                  .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-                  .forEach(tr => table.appendChild(tr) );
-          })));
-      </script>
   </head>
   <body>
       <h1>Trivy GitHub Vulnerability Report - ${title}</h1>
@@ -339,6 +327,18 @@ async function generateReportPage(title: string, viewName: string, repositories:
     )
     .join("\n")}
       </table>
+      <script type="text/javascript">
+          const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+          const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+              v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+              )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+          document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+              const table = th.closest('table');
+              Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+                  .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+                  .forEach(tr => table.appendChild(tr) );
+          })));
+      </script>
   </body>
   </html>
   `;
